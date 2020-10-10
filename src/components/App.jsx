2bf967fbd179a7725c70affabb74738cf96024aa
Sotaro Sakuyama <apmgjg124953@gmail.com>
2020-10-10 16:34:58 +0900

    少しの進捗

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
-import React, {component} from 'react';
+import React, { Component } from 'react';
 import Form from './Form';
+import List from './List';
 
-export default class App extends Components{
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo: []
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(e){
+    console.log(e.target.title.value);
+    e.preventDefault();
+  }
+  
   render(){
     return(
-      <div className="App">
+      <div className="siimple-box siimple--bg-dark">
+        <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+        <Form/>
+        <div className="siimple-rule"></div>
+        <List/>
       </div>
     );
   }
