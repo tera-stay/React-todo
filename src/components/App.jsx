88d5aa1ba24e85763c2b998d7d96324aa93bf23e
Sotaro Sakuyama <apmgjg124953@gmail.com>
2020-10-10 15:38:44 +0900

    製作開始

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, {component} from 'react';
+import Form from './Form';
+
+export default class App extends Components{
+  render(){
+    return(
+      <div className="App">
+      </div>
+    );
+  }
+}
